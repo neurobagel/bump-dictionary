0092e057c0b83e89741a9f3cf6a1b103326f509e
Alyssa Dai <alyssa.ydai@gmail.com>
2025-08-25 22:15:33 -0400

    rename bump-dictionary module to bump_dictionary and add dictionary models

---
+++ bump_dictionary/cli.py
@@ -0,0 +1,57 @@
+from pathlib import Path
+
+import typer
+import utils
+from logger import log_error, logger
+from models import new_dictionary_model, old_dictionary_model
+from typing_extensions import Annotated
+
+bump_dictionary = typer.Typer(
+    help="Bump Neurobagel data dictionaries to the latest version of the data dictionary schema.",
+    # context_settings={"help_option_names": ["-h", "--help"]},
+    rich_markup_mode="rich",
+)
+
+
+def main(
+    data_dictionary: Annotated[
+        Path,
+        typer.Argument(help="Path to a Neurobagel data dictionary JSON file."),
+    ],
+    output: Annotated[
+        Path, typer.Argument(help="Path to save the output JSON file.")
+    ] = Path("bumped_dictionary.json"),
+):
+    current_dict = utils.load_json(data_dictionary)
+
+    old_schema_validation_errs = utils.validate_against_dictionary_schema(
+        current_dict, old_dictionary_model.DataDictionary.model_json_schema()
+    )
+
+    if old_schema_validation_errs:
+        validation_errs = ""
+        for error in old_schema_validation_errs:
+            validation_errs += (
+                " ->" + ".".join(map(str, error.path)) + f": {error.message}\n"
+            )
+        log_error(
+            logger,
+            "The data dictionary is not valid against previous schema and may be too outdated to upgrade automatically. "
+            "Please re-annotate your dataset using the latest version of the annotation tool to continue.\n"
+            f"Found {len(old_schema_validation_errs)} error(s):\n"
+            f"{validation_errs}",
+        )
+
+    new_schema_validation_errs = utils.validate_against_dictionary_schema(
+        current_dict, new_dictionary_model.DataDictionary.model_json_schema()
+    )
+
+    if not new_schema_validation_errs:
+        logger.info(
+            "Data dictionary is already up-to-date with the latest schema."
+        )
+        raise typer.Exit(code=0)
+
+
+if __name__ == "__main__":
+    typer.run(main)
---
+++ bump_dictionary/utils.py
@@ -0,0 +1,35 @@
+import json
+from pathlib import Path
+from typing import Any
+
+from jsonschema import Draft202012Validator
+from logger import log_error, logger
+
+
+def load_json(file: Path) -> Any:
+    """Load a JSON file and return its content if file has valid encoding and is valid JSON."""
+    try:
+        with open(file, "r", encoding="utf-8") as f:
+            return json.load(f)
+    except UnicodeDecodeError:
+        log_error(
+            logger,
+            f"Data dictionary must have UTF-8 encoding: {file}. "
+            "Tip: Need help converting your file? Try a tool like iconv (http://linux.die.net/man/1/iconv) or https://www.freeformatter.com/convert-file-encoding.html.",
+        )
+    except json.JSONDecodeError:
+        log_error(
+            logger,
+            f"Data dictionary is not valid JSON: {file}.",
+        )
+
+
+def validate_against_dictionary_schema(
+    data_dictionary: dict, schema: dict
+) -> list:
+    """
+    Validate the data dictionary against a given schema and return all validation errors if any found.
+    """
+    validator = Draft202012Validator(schema)
+    errors = list(validator.iter_errors(data_dictionary))
+    return errors
