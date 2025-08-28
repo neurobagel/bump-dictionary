40d3e57d5b191082e0157035dd8185e34892402b
Alyssa Dai <alyssa.ydai@gmail.com>
2025-08-28 15:40:11 -0400

    [ENH] Typer app to remove deprecated `Identifies` and add `VariableType` to old data dictionaries (#3)
    * add pyproject.toml
    
    * add flake8 config and pre-commit config
    
    * add basic Typer app
    
    * add logger
    
    * add basic README
    
    * add logic to add VariableType
    
    * add verbosity and overwrite options
    
    * add custom .autorc and release.yaml for PyPI
    
    * add dev environment setup to README
    
    * add installation and usage instructions
    
    * test bump-dictionary using example JSON files
    
    * add test for Transformation -> Format
    
    * add badges
    
    * use 'legacy' and 'latest' everywhere
    
    * update help and add all maintainers' names
    
    * bake format/transformation validation into Pydantic model and remove check_overwrite utility
    
    ---------
    
    Co-authored-by: Sebastian Urchs <surchs@users.noreply.github.com>

---
+++ tests/test_bump_dictionary.py
@@ -0,0 +1,139 @@
+import json
+from pathlib import Path
+
+import pytest
+from typer.testing import CliRunner
+
+from bump_dictionary.cli import bump_dictionary
+
+
+@pytest.fixture(scope="session")
+def runner():
+    return CliRunner()
+
+
+@pytest.fixture(scope="session")
+def example_dictionaries_path():
+    return Path(__file__).absolute().parent / "data"
+
+
+@pytest.fixture(scope="function")
+def example_output_path(tmp_path):
+    return tmp_path / "updated_dictionary.json"
+
+
+@pytest.fixture(scope="session")
+def load_test_json():
+    def _read_file(file_path):
+        with open(file_path, "r") as f:
+            return json.load(f)
+
+    return _read_file
+
+
+def test_valid_legacy_dictionary_upgraded(
+    load_test_json, example_dictionaries_path, runner, example_output_path
+):
+    """Test that a data dictionary valid against the legacy schema is upgraded correctly."""
+    target_latest_dict = load_test_json(
+        example_dictionaries_path / "latest_schema_dictionary.json"
+    )
+
+    result = runner.invoke(
+        bump_dictionary,
+        [
+            str(example_dictionaries_path / "legacy_schema_dictionary.json"),
+            str(example_output_path),
+        ],
+    )
+
+    output = load_test_json(example_output_path)
+
+    assert result.exit_code == 0
+    assert output == target_latest_dict
+
+
+def test_valid_legacy_dictionary_with_transformation_upgraded(
+    load_test_json,
+    example_dictionaries_path,
+    runner,
+    example_output_path,
+    caplog,
+):
+    """
+    Test that a valid legacy data dictionary containing 'Transformation' for continuous columns
+    is upgraded correctly, with 'Transformation' renamed to 'Format'.
+    """
+    target_latest_dict = load_test_json(
+        example_dictionaries_path / "latest_schema_dictionary.json"
+    )
+
+    result = runner.invoke(
+        bump_dictionary,
+        [
+            str(
+                example_dictionaries_path
+                / "legacy_schema_dictionary_with_transformation.json"
+            ),
+            str(example_output_path),
+        ],
+    )
+
+    output = load_test_json(example_output_path)
+
+    assert result.exit_code == 0
+    assert "Renaming 'Transformation' to 'Format'" in caplog.text
+    age_col = next(
+        (
+            col
+            for col in output.values()
+            if col.get("Annotations", {}).get("IsAbout", {}).get("TermURL")
+            == "nb:Age"
+        ),
+    )
+    assert "Format" in age_col["Annotations"]
+    assert output == target_latest_dict
+
+
+def test_valid_latest_dictionary_not_upgraded(
+    example_dictionaries_path, runner, caplog
+):
+    """
+    Test that a data dictionary valid against the latest schema is not upgraded,
+    and the app exits with an informative error and no output.
+    """
+    result = runner.invoke(
+        bump_dictionary,
+        [
+            str(example_dictionaries_path / "latest_schema_dictionary.json"),
+            str(example_output_path),
+        ],
+    )
+
+    assert result.exit_code != 0
+    assert len(caplog.records) == 1
+    assert "already up-to-date" in caplog.text
+
+
+def test_invalid_dictionary_not_upgraded(
+    example_dictionaries_path, runner, caplog
+):
+    """
+    Test that a data dictionary which is not valid against the legacy schema is not upgraded,
+    with an informative error.
+    """
+    result = runner.invoke(
+        bump_dictionary,
+        [
+            str(example_dictionaries_path / "invalid_dictionary.json"),
+            str(example_output_path),
+        ],
+    )
+
+    assert result.exit_code != 0
+    assert len(caplog.records) == 1
+    assert "not valid against the legacy schema" in caplog.text
+    assert "3 error(s)" in caplog.text
+    assert all(
+        col in caplog.text for col in ["participant", "session", "pheno_age"]
+    )
